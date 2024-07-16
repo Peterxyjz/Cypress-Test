277fe4fe46439a7d4fce981a07d2b5913c36e02c
Peterxyjz <huyq09@gmail.com>
2024-07-16 11:48:35 +0700

    first commit

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,15 @@
+describe('Test login ', () => {
+  //test case 1
+  it('Test case 1: login fail, email or password is not correct', () => {
+    cy.viewport(1536, 960)
+    cy.visit('http://localhost:3000/login');
+    cy.wait(5000)
+    cy.get('#email').click().type('phong@gmail.com')
+    cy.wait(3000)
+    cy.get('#password').click().type('phong@gmail.com')
+    cy.wait(3000)
+    cy.get('.bg-violet-500').click()
+    cy.wait(3000)
+    cy.get('.text-red-600').should('contain', 'Email hoặc mật khẩu không đúng')
+  })
+})  
